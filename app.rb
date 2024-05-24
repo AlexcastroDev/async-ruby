51d8707a43a7ba2a2348def77ed1addf7f617e4a
Alexandro Castro <10711649+AlexcastroDev@users.noreply.github.com>
2024-05-24 20:42:51 +0000

    fix: async

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ gemfile(true) do
   git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
   gem "activerecord", "7.1.2"
-  gem "pg"
+  gem "sqlite3", "1.4.2"
   gem 'async'
   gem "faker"
 end
@@ -21,18 +21,15 @@ require "async"
 
 # This connection will do for database-independent bug reports.
 ActiveRecord::Base.establish_connection(
-  adapter: "postgresql",
+  adapter: "sqlite3",
   encoding: "unicode",
-  database: ENV["DB_NAME"],
-  username: "postgres",
-  password: "",
-  host: ENV["DB_HOST"],
+  database: ":memory:",
 )
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveRecord::Schema.define do
-  create_table :items, id: :serial, force: true do |t|
+  create_table :items, force: true do |t|
     t.string :title
     t.string :description
   end
@@ -49,34 +46,46 @@ class OpenAILib
 end
 
 class BugTest < Minitest::Test
-  def test_async_task
+  def test_create_unverified_user
     items = []
     start_time = Time.now
 
+    # Ensure the CSV file exists
+    unless File.exist?('items.csv')
+      raise "CSV file not found"
+    end
+
+    # Use Async to run the generation tasks concurrently
     Async do |task|
       tasks = []
 
       CSV.foreach('items.csv', encoding: 'utf-8', headers: true, skip_blanks: true, col_sep: ',').with_index(1) do |row, index|
         tasks << task.async do
-          item = Item.new(title: row['title'])
-          item.description = OpenAILib.new.generate_description
+          item = { title: row['title'], description: OpenAILib.new.generate_description }
           items << item
         end
       end
 
+      # Await all tasks to complete
       tasks.each(&:wait)
     end
 
-    Item.transaction do
-      items.each(&:save!)
+    # Check if there are any items to save
+    if items.empty?
+      puts "No items to save"
+      return
     end
+
+    # Save all items to the database after generating descriptions in a single insert
+    Item.create!(items)
 
     end_time = Time.now
     elapsed_time = end_time - start_time
     puts "Elapsed time: #{elapsed_time} seconds"
 
-    puts "Items count: #{Item.count}"
-
-    assert_equal 1000, Item.count
+    # Assert the item count equals 1000
+    total_items = Item.count
+    puts "Total items: #{total_items}"
+    assert_equal 1000, total_items
   end
 end
